aa72accdf776dc2d6ea6b248848e48f45ba4fec3
Andrea Faundez <afaundezd@gmail.com>
2022-07-14 09:34:24 +1200

    install bootstrap

--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
+import 'bootstrap/dist/css/bootstrap.css'
 
 import reducers from './reducers'
 import App from './components/App'
