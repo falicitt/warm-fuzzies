0f9a1a62282a95ada939d81045b1e84331e93ec4
Andrea Faundez <103091090+andrea-faundez@users.noreply.github.com>
2022-07-14 14:56:24 +1200

    post route for card and insert db function

---
+++ server/db/DB-Functions/cards.js
@@ -0,0 +1,14 @@
+const conn = require('../connection')
+
+function insertCard(newCard, db = conn) {
+  return db('cards').insert(newCard)
+}
+
+function getCardById(id, db = conn) {
+  return('cards').where('id', id).first().select()
+}
+
+module.exports = {
+  insertCard,
+  getCardById
+}
