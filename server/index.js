a7c9a29fc87fd0c103d6ea015d7501e5e23c28e2
Chris-Warne <chrisklinx@msn.com>
2022-07-20 09:19:49 +1200

    packagejason

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const server = require('./server')
 
-const PORT = process.env.PORT || 3000
+ const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => {
   // eslint-disable-next-line no-console
