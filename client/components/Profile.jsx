cba7a36943b0720642cf8b98438e80790bd0a031
yayuan_cassie <84106763+falicitt@users.noreply.github.com>
2022-07-20 09:41:18 +1200

    finish profile page no css

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from 'react-redux'
 import { getCardsByUser } from '../apis/cards'
+import { Link } from 'react-router-dom'
 
 function Profile() {
   const email = useSelector(state => state.loggedInUser.email)
@@ -14,13 +15,24 @@ function Profile() {
         setCards(cardsArry)
       })
   }, [auth0Id])
+
+  const tf = new Intl.DateTimeFormat('en-NZ')
   
   return (
     <>
-    <h1>My Profile</h1>
-      <p>Logged In Email: {email}</p>
+      <h1>My Profile</h1>
+      <p>You are logged in as: {email}</p>
       <h2>My Cards</h2>
-      {cards?.map(card => <li key={card.id}>{card.name}</li>)}
+      {cards?.map(card => 
+      
+        <li key={card.id}>
+          <Link to={`/card/${card.id}`}>
+          <p>{card.name} {card.person_name}</p>
+          <p>{tf.format(new Date(card.created_at))}</p>
+          </Link>
+        </li>
+       
+        )}
     </>
   )
 }
