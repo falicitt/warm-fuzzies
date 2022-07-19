6cff67296718026ed747c01490007f63b8d96e02
Chris-Warne <chrisklinx@msn.com>
2022-07-20 11:38:59 +1200

    production function added for fix3

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -25,8 +25,10 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
     pool: {
       min: 2,
       max: 10
