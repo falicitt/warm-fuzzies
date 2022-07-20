cad4a9be5fa5b884740030a0193328fb9a5abd26
Andrea Faundez <afaundezd@gmail.com>
2022-07-20 20:07:36 +1200

    done

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -58,15 +58,13 @@ function Nav() {
     <>
     <div>
     <div className="container">
-    <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-      <a href="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-      <a className="poto1" href="/"><img src="/smallLogo.svg" alt="logo" className='logo-img my-2' height="60"/></a>
-      </a>
-
-      <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-      </ul>
-
-      <div className="col-md-3 text-end poto2">
+    <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4">
+      <div className='caca'>
+      <div>
+        <a href="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+        <a className="poto1" href="/"><img src="/smallLogo.svg" alt="logo" className='logo-img my-2' height="60"/></a></a>
+      </div>
+      <div className="col-md-3 text-end">
       <IfAuthenticated>
           <a href="/profile" className="btn btn-light btn-sm mx-2" role="button">
             My Profile
@@ -82,6 +80,7 @@ function Nav() {
             Log in
           </a>
        </IfNotAuthenticated>
+      </div>
       </div>
     </header>
   </div>
